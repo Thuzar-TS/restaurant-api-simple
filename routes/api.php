372239599c4a2b2520eb059d026752be896deb74
Thuzar-TS <thuzar.ts92@gmail.com>
2021-11-23 11:18:08 +0630

    route and crud controller

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// for api version 1
+Route::prefix('v1')->group(function(){
+    Route::get('/restaurants', 'V1\RestaurantController@list');
+    Route::get('/restaurants/{restaurantId}', 'V1\RestaurantController@getRestaurant');
+    Route::post('/restaurants', 'V1\RestaurantController@createRestaurant');
+    Route::put('/restaurants/{restaurantId}', 'V1\RestaurantController@updateRestaurant');
+    Route::delete('/restaurants/{restaurantId}', 'V1\RestaurantController@deleteRestaurant');
 });
